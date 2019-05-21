bf2b027331d49d76d9672840373a2f122fd07819
Bruno Oliveira <bruno@sistemasbr.net>
2019-05-21 10:05:13 -0300

    Subindo app ionic

---
+++ ionic/ionicFirebase/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBeetF0E4lDon-Op9aeOWaeqMQFduAsD9U",
+    authDomain: "appexemplo-c963a.firebaseapp.com",
+    databaseURL: "https://appexemplo-c963a.firebaseio.com",
+    projectId: "appexemplo-c963a",
+    storageBucket: "appexemplo-c963a.appspot.com",
+    messagingSenderId: "549662839209",
+    appId: "1:549662839209:web:89031a8fe070d07c"
+};
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, 
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ ionic/ionicFirebase/src/app/home/home.page.ts
@@ -0,0 +1,76 @@
+import { Component, ViewChild } from '@angular/core';
+import { NavController, ToastController } from '@ionic/angular';
+import { AngularFireAuth } from '@angular/fire/auth';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  public user: any;
+  @ViewChild('usuario') email;
+  @ViewChild('senha') password;
+constructor(public navCtrl: NavController,
+              public toastCtrl: ToastController,
+              public firebaseauth: AngularFireAuth) {
+      firebaseauth.user.subscribe((data => {
+        this.user = data;
+      }));     
+  }
+
+public LoginComEmail(): void {
+    this.firebaseauth.auth.signInWithEmailAndPassword(this.email.value , this.password.value)
+      .then(() => {
+        this.exibirMensagem("Logado com sucesso");
+       })
+        .catch((erro: any) => {
+          this.exibirMensagem("Não há registro de usuário correspondente a esse identificador");
+      });
+  }
+
+  async exibirMensagem(mensagem) {
+    const toast = await this.toastCtrl.create({
+      message: mensagem, duration: 2000
+    });
+    toast.present();
+  }
+  
+public cadastrarUsuario(): void {
+    this.firebaseauth.auth.createUserWithEmailAndPassword(this.email.value , this.password.value)
+    .then(() => {
+      this.exibirMensagem("Usuário criado com sucesso");
+    })
+    .catch((erro: any) => {
+      this.exibirMensagem(erro)
+    });
+  }
+
+public alterarEmail(): void {
+    var user = this.firebaseauth.auth.currentUser;
+    user.updateEmail(this.email.value).then(function() {
+      this.exibirMensagem("Usuário alterado com sucesso")
+    }).catch(function(error) {
+      this.exibirMensagem(error)
+    });
+}
+
+public excluirUsuario(): void {
+  var user = this.firebaseauth.auth.currentUser;
+    user.delete().then(function() {
+      this.exibirMensagem("Usuário excluido com sucesso")      
+    }).catch(function(error) {
+      this.exibirMensagem(error)
+    });
+}
+
+public Sair(): void {
+    this.firebaseauth.auth.signOut()
+    .then(() => {
+      this.exibirMensagem("Saiu com sucesso");  
+    })
+    .catch((erro: any) => {
+      this.exibirMensagem(erro)
+    });
+  }
+}
